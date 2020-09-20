50a46552a5dddf89f0128cd9c5c05f9b9f59a8d9
ColinHan <mr.colinhan@gmail.com>
2020-09-20 17:38:23 -0500

    adding add logs action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
 
 // Before refactor
 // export const getLogs = () => {
@@ -20,6 +20,25 @@ export const getLogs = () => async dispatch => {
     const res = await fetch('/logs');
     const data = await res.json();
     dispatch({ type: GET_LOGS, payload: data });
+  } catch (error) {
+    dispatch({ type: LOGS_ERROR, payload: error.response.data });
+  }
+};
+
+// Add Logs to server
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+    dispatch({ type: ADD_LOG, payload: data });
   } catch (error) {
     dispatch({ type: LOGS_ERROR, payload: error.response.data });
   }
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -30,7 +30,8 @@ export const Logs = ({ log: { logs, loading }, getLogs }) => {
 };
 
 Logs.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
